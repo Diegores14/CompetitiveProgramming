de9f1dbe7eebbafbaa0006fee514d6109d69977f
Diegores14 <diegorestrepo68@utp.edu.co>
2018-08-04 23:15:47 -0500

    1931

--- Uri/graph/1931 - Even Obsession/solution.cpp
+++ Uri/graph/1931 - Even Obsession/solution.cpp
@@ -3,42 +3,45 @@ using namespace std;
 typedef pair<int,int> ii;
 
 struct node{
-	int b;
-	long long c;
-	bool p;
-	set<ii> m;
-	node(int _b, long long _c,bool _p, set<ii> _m){b=_b; c=_c; p=_p; m=_m;}
+	int first;
+	long long second;
+	node(int a, long long b){ first = a; second = b;}
 };
 
 bool operator <(node a, node b){
-	return a.c > b.c;
-};
+ 	return a.second > b.second;
+}
+
+long long dijkstra(vector<list<ii>> & v){
+	vector<bool> used(v.size(), false);
+	priority_queue<node> q;
+	q.push(node(0,0));
+	while(!q.empty() && q.top().first!=(int)v.size()-1){
+	 	int a = q.top().first;
+	 	long long b = q.top().second;
+	 	q.pop();
+	 	if(!used[a]){
+	 		used[a] = true;
+		 	for(ii to : v[a]){
+	 		 	for(ii i: v[to.first])
+		 	 		if(!used[i.first])
+		 	 			q.push(node(i.first, b + to.second + i.second));
+		 	}
+	 	}
+	}
+	return (q.empty())? -1 : q.top().second;
+}
 
 int main(){
 	ios_base::sync_with_stdio(false); cin.tie(NULL);
-	int n, m, a, b, c;
-	cin >> n >> m;
-	vector<list<ii>> g(n);
-	for(int i=0; i<m; i++){ cin >> a >> b >> c; g[a-1].push_back(ii(b,c)); g[b-1].push_back(ii(a,c));}
-	priority_queue<node> q;
-	set<ii> s, aux;
-	long long d;
-	bool p;
-	q.push(node(0, 0, false, s));
-	while(!q.empty() && (!q.top().p || q.top().b != n-1)){
-		a = q.top().b;
-		d = q.top().c;
-		p = q.top().p;
-		aux = q.top().m;
-		q.pop();
-		for(ii to : g[b])
-			if(aux.count(ii(a,to.first))==0){
-				set<ii> aux1 = aux;
-			 	aux1.insert(ii(a,to.first));
-			 	q.push(node(to.first, d+to.second, !p, aux1));
-			}
+	int c, v, a, b, d;
+	cin >> c >> v;
+	vector<list<ii>> g(c);
+	for(int i=0; i<v; i++){
+	 	cin >> a >> b >> d;
+	 	g[a-1].push_back(ii(b-1, d));
+	 	g[b-1].push_back(ii(a-1, d));
 	}
-	if(q.empty()) cout << "-1\n";
-	else cout << q.top().c << '\n';
+	cout << dijkstra( g) << '\n';
 	return 0;
 }
