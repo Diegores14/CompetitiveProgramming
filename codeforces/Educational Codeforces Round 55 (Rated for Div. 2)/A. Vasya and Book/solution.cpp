a0605fd006125b33a7e4ad2fdfe56a3f0e6ea67c
Diegores14 <diegorestrepo68@utp.edu.co>
2018-11-29 10:56:50 -0500

    codeforces educational round 55

--- codeforces/Educational Codeforces Round 55 (Rated for Div. 2)/A. Vasya and Book/solution.cpp
+++ codeforces/Educational Codeforces Round 55 (Rated for Div. 2)/A. Vasya and Book/solution.cpp
@@ -1,78 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+long long solve(long long a, long long b, long long d){
+    if(abs(a-b)%d)
+        return 4e10;
+    else
+        return abs(a-b)/d;
+}
+
+long long solve2(long long a, long long b, long long d){
+    return abs(a-b)/d + (abs(a-b)%d > 0);
+}
+
 int main(){
     ios_base::sync_with_stdio(false); cin.tie(NULL);
-    int t, n, x, y, d;
+    long long t, n, x, y, d;
     cin >> t;
     while(t--){
         cin >> n >> x >> y >> d;
-        long long aux = x, a=0, b = 0;
-        bool flag1 = false, flag2 = false;
-        while(aux>1){
-            if(aux == y){
-                flag1 = true;
-                break;
-            }
-            aux -= d;
-            a++;
-        }
-        aux = 1;
-        if(aux == y){
-            flag1 = true;
-        }
-        if(!flag1)
-        while(aux < n){
-            a++;
-            aux += d;
-            if(aux == y){
-                flag1 = true;
-                break;
-            }
-        }
-        aux = n;
-        if(aux == y){
-            flag1 = true;
-        }
-        aux = x;
-        while(aux<n){
-            if(aux == y){
-                flag2= true;
-                break;
-            }
-            b++;
-            aux += d;
-        }
-        aux = n;
-        if(aux == y){
-            flag2 = true;
-        }
-        if(!flag2)
-        while(aux>1){
-            b++;
-            aux -= d;
-            if(aux == y){
-                flag2= true;
-                break;
-            }
-        }
-        aux == 1;
-        if(aux == y){
-            flag2 = true;
-        }
-        if(flag1 && flag2){
-           cout << min(a, b) << '\n';
-           continue;
-        }
-        if(flag1){
-            cout << a << '\n';
-            continue;
-        }
-        if(flag2){
-            cout << b << '\n';
-            continue;
-        }
-        cout << "-1\n";
+        long long ans = 2e10;
+        ans = min(ans, solve(x,y,d));
+        ans = min(ans, solve2(x,1,d) + solve(y, 1, d)) ;
+        ans = min(ans, solve2(x,n,d) + solve(y, n, d));
+        if(ans >= 2e10)
+            cout << "-1\n";
+        else
+            cout << ans << '\n';
     }
     return 0;
 }
