8b57b07153c89f14fb44c1d414d9f7a7fdde4a6b
Diegores14 <diegorestrepo68@utp.edu.co>
2018-12-27 14:58:43 -0500

    Solution codeforces Round #529 (Dic. 3)

---
+++ codeforces/Codeforces Round #529 (Div. 3)/D. Circular Dance/solution.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> ans(n+1);
+    vector<pair<int,int>> v(n+1);
+    for(int i=1; i<=n; i++){
+        cin >> v[i].first >> v[i].second;
+    }
+    for(int i=1; i<=n; i++){
+        if(ans[v[i].first] == 0 && (v[v[i].first].first == v[i].second || v[v[i].first].second == v[i].second)){
+            ans[v[i].first] = v[i].second;
+        }else{
+            ans[v[i].second] = v[i].first;
+        }
+    }
+    cout << ans[1];
+    int i = ans[1];
+    while(i != 1){
+        cout << ' ' << ans[i];
+        i = ans[i];
+    }
+    cout << '\n';
+    return 0;
+}
