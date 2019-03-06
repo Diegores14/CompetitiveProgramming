a6a7da717b3bcf4916982014bc1adf8a72737691
Diegores14 <diegorestrepo68@utp.edu.co>
2019-03-06 06:34:49 -0500

    Solution problem C Educational Codeforces Round 61

--- codeforces/Educational Codeforces Round 61 (Rated for Div. 2)/C. Painting the Fence/solution.cpp
+++ codeforces/Educational Codeforces Round 61 (Rated for Div. 2)/C. Painting the Fence/solution.cpp
@@ -6,25 +6,26 @@ int main() {
     ios_base::sync_with_stdio(false); cin.tie(NULL);
     memset(desc, 0, sizeof desc);
     memset(desc1, 0, sizeof desc1);
-    vector< vector<int> > v(5005);
-    int n, q;
+    int v[5005][4];
+    memset( v, 0, sizeof v);
+    int n, q, a, b;
     long long total = 0;
     cin >> n >> q;
-    vector< pair<int, int> > painters(q);
-    int l=0;
-    for(auto &i: painters) {
-        cin >> i.first >> i.second;
-        for(int j=i.first; j<=i.second; j++) {
-            v[j].emplace_back(l);
-            total += (v[j].size() == 1);
+    for(int i=0; i<q; i++) {
+        cin >> a >> b;
+        for(int j=a; j<=b; j++) {
+            if(v[j][3] < 3) {
+                v[j][v[j][3]] = i;
+                v[j][3]++;
+                total += (v[j][3] == 1);
+            }
         }
-        l++;
     }
     for(int i=0; i<=5000; i++) {
-        if(v[i].size() == 1) {
+        if(v[i][3] == 1) {
             desc[v[i][0]]++;
         }
-        if(v[i].size() == 2) {
+        if(v[i][3] == 2) {
             desc1[v[i][0]][v[i][1]]++;
         }
     }
