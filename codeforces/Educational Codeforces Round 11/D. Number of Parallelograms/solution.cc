5cbc52ac6a4c1fe8310384375aaea9e553c370a4
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2019-08-11 22:10:14 -0500

    Solution problem D Educational Codeforces Round 11

---
+++ codeforces/Educational Codeforces Round 11/D. Number of Parallelograms/solution.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef pair< int, int > ii;
+
+void solve( ) {
+    int n;
+    cin >> n;
+    vector< ii > v(n);
+    vector< ii > m;
+    for( int i = 0 ; i < n; i++ ) {
+        cin >> v[i].first >> v[i].second;
+        for( int j = 0; j < i; j++ ) {
+            m.push_back( ii(v[i].first + v[j].first, v[i].second + v[j].second) );
+        }
+    }
+    sort( m.begin(), m.end() );
+    int ans = 0, count = 1;
+    for( int i = 1; i < m.size(); i++ ) {
+        if( m[i] == m[i-1]) {
+            count++;
+        } else {
+            ans += ( (count - 1)*(count) )/2;
+            count = 1;
+        }
+    }
+    cout << ans << '\n';
+}
+
+int main() {
+    ios_base::sync_with_stdio(); cin.tie(NULL);
+    solve();
+    return 0;
+}
