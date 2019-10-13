b8f1217f1da187bc9678a3aa56e84cd6f0811971
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2019-10-12 19:07:27 -0500

    Solution problem 1248 LigthOJ

---
+++ Lightoj/1248 - Dice (III)/solution.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void solve() {
+    int n; 
+    cin >> n;
+    double ans = 0;
+    for( int i = n-1; i >= 0; i-- ) {
+        ans += double(n)/(n-i);
+    }
+    cout << ans << '\n';
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(false); cin.tie(NULL);
+    int t;
+    cout << fixed << setprecision(10);
+    cin >> t;
+    for( int cases = 1; cases <= t; cases++ ) {
+        cout << "Case " << cases << ": ";
+        solve();
+    }
+    return 0;
+}
