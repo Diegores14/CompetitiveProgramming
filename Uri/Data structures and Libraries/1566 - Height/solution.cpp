60858518903ab2397c6a89c6cb081d57492a3a09
Diego Alejandro Restrepo Sanchez <diegorestrepo68@utp.edu.co>
2019-10-23 09:24:44 -0500

    Solution Height 1566

--- Uri/Data structures and Libraries/1566 - Height/solution.cpp
+++ Uri/Data structures and Libraries/1566 - Height/solution.cpp
@@ -1,18 +1,25 @@
 #include <bits/stdc++.h>
 using namespace std;
+int h[3000001];
 
 int main(){
-	ios_base::sync_with_stdio(false);cin.tie(NULL);
-	int n, p;
-	cin >> n;
+	int n, p, temp;
+	int v[211];
+	scanf("%d", &n);
 	while(n--){
-	 	cin >> p;
-		vector<int> h(p);
-		for(int i=0; i<p; i++){
-		 	cin >> h[i];
+	 	scanf("%d", &p);
+		memset( v, 0, sizeof v );
+		for(int i=0; i<p; i++) {
+		 	scanf("%d", &temp);
+			v[temp-20]++;
+		}		 
+		for( int i = 0, j = 0; i <= 210 && j < p; i++) {
+			while( v[i]-- ) {
+				h[j++] = i+20;
+			}
 		}
-		sort(h.begin(),h.end());
-		for(int i=0; i<p-1; i++)cout << h[i] << ' ';
-		cout << h[p-1] <<'\n';
+		for(int i=0; i<p-1; i++)printf("%d ", h[i]);
+		printf("%d\n", h[p-1]);
 	}
+	return 0;
 }	
